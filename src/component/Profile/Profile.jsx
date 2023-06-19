cab6c721e90ec3be69bab80fd4bc068918203b7e
Neat <sophaneat@student.fdu.edu>
2023-06-19 10:25:38 -0700

    Add User Profile Component

--- src/component/Profile/Profile.jsx
+++ src/component/Profile/Profile.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
-const Profile = () => {
+import React from 'react';
+import "./Profile.scss";
+import profile from "../../asset/images/profile.png";
+const Profile = ({user}) => {
     return (
-        <div>Profile</div>
+        <div className="profile">
+            <div className="profile__banner">
+                <img src={profile} alt="User Profile" className="profile__img" />
+            </div>
+            <div className="profile__content">
+                <div className="profile__desc">
+                    <p className="profile__name">{user.firstName} {user.lastName}</p>
+                    <p className="profile__bio">I love programming to heart.</p>
+                </div>
+                <div className="profile__desc2">
+                    <p className="profile__text">{user?.university ? user.university : "Fairleigh Dickinson University"}</p>
+                    <p className="profile__text">{user?.degree ? user.degree : "B.S. in Information Technology"}</p>
+                </div>
+                <div className="profile__desc3">
+                    <p className="profile__text">Your connections: {user.friends.length}</p>
+                    <p className="profile__text">Your course: {user.enrollment.length}</p>
+                </div>
+            </div>
+        </div>
     )
 }
-
 export default Profile
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
+import React from 'react';
+import "./Dashboard.scss";
 import { useContext } from 'react';
 import { UserContext } from '../../App';
 // Component
 import Profile from '../../component/Profile/Profile';
-
+import UploadPost from '../../component/UploadPost/UploadPost';
+import SetUp from '../../component/SetUp/SetUp';
+import Courses from '../../component/Courses/Courses';
 const Dashboard = () => {
     const user = useContext(UserContext);
+    console.log(user);
     return (
         <div className="dashboard">
             <div className="dashboard__col1">
-                <Profile />
+                <Profile user={user}/>
             </div>
             <div className="dashboard__col2">
-
+                {/* New User */}
+                {
+                    user?.degree ? (
+                        <UploadPost />
+                    ) : (
+                        <SetUp />
+                    )
+                }
             </div>
             <div className="dashboard__col3">
-
+                <Courses />
             </div>
         </div>
     )
