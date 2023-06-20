bf90f6331dd8cd0f39cca9353e0a18f5faa7dd2d
Neat <sophaneat@student.fdu.edu>
2023-06-20 15:57:26 -0700

    Add posting post functionality

--- src/component/PostModal/PostModal.jsx
+++ src/component/PostModal/PostModal.jsx
@@ -2,11 +2,34 @@ import React from 'react'
 import "./PostModal.scss";
 import { useContext, useState } from 'react';
 import { UserContext } from '../../App';
+
 // Icon and image
 import profile from "../../asset/images/profile.png";
 import cross from "../../asset/icon/x-button.png";
+
+import axios from "axios";
+
 const PostModal = ({setIsModalOpen}) => {
     const User = useContext(UserContext);
+    const [content, setContent] = useState("");
+    const [success, setSuccess] = useState("Post");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(content.length > 0) {
+            axios.post(`${process.env.REACT_APP_BASE_URL}/post`, {
+                userID: User._id,
+                content})
+                .then((response) => {
+                    setSuccess("Posted");
+                    setTimeout(() => {
+                        setIsModalOpen(false);
+                    }, 2000)
+                })
+                .catch(()=> {
+                    setSuccess("Unable to post")
+                })
+        }
+    }
     return (
         <div className="postModal">
             {/* Content */}
@@ -30,11 +53,11 @@ const PostModal = ({setIsModalOpen}) => {
                 </div>
                 {/* Input Section */}
                 <div className="postModal__input__section">
-                    <input className="postModal__input" type="text" placeholder='What do you want to talk about'/>
+                    <input value={content} onChange={(e) => setContent(e.target.value)} className="postModal__input" type="text" placeholder='What do you want to talk about'/>
                 </div>
                 {/* Post Button */}
                 <div className="postModal__action">
-                    <button className="postModal__post__btn">Post</button>
+                    <button onClick={(e)=>handleSubmit(e)} className="postModal__post__btn">{success}</button>
                 </div>
             </div>
             
